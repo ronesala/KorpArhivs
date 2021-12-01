94285d3215372932b27f12ced41202ce0a282188
ronesala <ronesala@gmail.com>
2021-12-01 16:20:31 +0200

    Pievienota datu izguve no datbuāzes

--- KorpArhivs/KorpArhivs/Pages/Images.cshtml.cs
+++ KorpArhivs/KorpArhivs/Pages/Images.cshtml.cs
@@ -1,12 +1,96 @@
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace KorpArhivs.Pages
 {
     public class ImagesModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        [BindProperty]
+        public List<Gallery> Galleries { get; set; }
+
+        public ImagesModel(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
+            //Nesapratu kaa pareizi pievienot...
+
+            var tableName = _configuration["StorageTables:Events"];
+            var connectionString = _configuration.GetConnectionString("TableStorage");
+
+            var tableClient = new TableClient(connectionString, tableName);
+
+            var events = tableClient.Query<TableEntity>();
+
+            Galleries = new List<Gallery>();
+
+            foreach (var gallery in events)
+            {
+                Galleries.Add(new Gallery
+                {
+                    EventName = gallery.GetString("Name"),
+                    EventDate = gallery.GetDateTimeOffset("Date").Value,
+                    EventDescription = gallery.GetString("Description"),
+                    Keyword = gallery.GetString("Keyword"),
+                    EventGroup = gallery.GetString("Group"),
+                });
+            }
+
+            //Events = new List<Event>
+            //{
+                //new Event
+                //{
+                //    EventName = "Pirmais notikums",
+                //    EventDate = new DateTime(2020, 08, 13),
+                //    EventDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                //    Keyword = "",
+                //    EventGroup = "Bildes"
+                //},
+                //new Event
+                //{
+                //    EventName = "Otrais notikums",
+                //    EventDate = new DateTime(2000, 07, 03),
+                //    EventDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                //    Keyword = "",
+                //    EventGroup = "Bildes"
+                //},
+                //new Event
+                //{
+                //    EventName = "Tresais notikums",
+                //    EventDate = new DateTime(2018, 10, 20),
+                //    EventDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                //    Keyword = "",
+                //    EventGroup = "Bildes"
+                //},
+                //new Event
+                //{
+                //    EventName = "Ceturtais notikums",
+                //    EventDate = new DateTime(2001, 02, 28),
+                //    EventDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                //    Keyword = "",
+                //    EventGroup = "Bildes"
+                //}
+
+            //};
+
         }
+    }
+
+    public class Gallery
+    {
+        public string EventName { get; set; }
+        public System.DateTimeOffset EventDate { get; set; }
+        public string EventDescription { get; set; }
+        public string Keyword { get; set; }
+        public string EventGroup { get; set; }
+
     }
 }
