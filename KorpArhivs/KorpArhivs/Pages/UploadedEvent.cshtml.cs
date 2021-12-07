7473d55359e6ee04af9875d031760a7456c60f82
ronesala <ronesala@gmail.com>
2021-12-07 16:30:12 +0200

    Strādājoša upload form. Konkrētas galerijas skati

---
+++ KorpArhivs/KorpArhivs/Pages/UploadedEvent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KorpArhivs.Pages
+{
+    public class UploadedEventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
