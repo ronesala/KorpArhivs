e9a3a7070bafce8414acb352c7f278cf9e33da31
ronesala <ronesala@gmail.com>
2021-12-20 16:48:40 +0200

    Rediģēju profila informācijas skatus un piuevienotju tiem pogu.

--- KorpArhivs/KorpArhivs/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ KorpArhivs/KorpArhivs/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -35,18 +35,18 @@ namespace KorpArhivs.Areas.Identity.Pages.Account.Manage
         {
             [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
+            [Display(Name = "Vecā parole")]
             public string OldPassword { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "Jaunā parole")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Display(Name = "Jaunā parole atkārtoti")]
+            [Compare("NewPassword", ErrorMessage = "Jaunā parole un tās atkārtojums nesakrīt")]
             public string ConfirmPassword { get; set; }
         }
 
