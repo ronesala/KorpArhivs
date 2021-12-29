2aa980bb8966ecd89ace7d87f9c651129f6841fc
ronesala <ronesala@gmail.com>
2021-12-29 14:14:19 +0200

    Ir iespēja pilnībā izdzēst notikumu caur "DeleteEvent" skatu

--- KorpArhivs/KorpArhivs/Pages/DeleteEventConfirmation.cshtml.cs
+++ KorpArhivs/KorpArhivs/Pages/DeleteEventConfirmation.cshtml.cs
@@ -14,13 +14,23 @@ namespace KorpArhivs.Pages
     {
         private readonly IConfiguration _configuration;
 
+        private BlobContainerClient _blobContainerClient;
+
+        private TableClient _tableClient;
+
+        private string _connectionString;
+
         public DeleteEventConfirmationModel(IConfiguration configuration)
         {
             _configuration = configuration;
-        }
+            var containerName = _configuration["StorageBlobs:Events"];
+            _connectionString = _configuration.GetConnectionString("TableStorage");
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+            _blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var tableName = _configuration["StorageTables:Events"];
+            _tableClient = new TableClient(_connectionString, tableName);
 
-        public string Category { get; set; }
-        public string Id { get; set; }
+        }
 
         [BindProperty]
         public string EventName { get; set; }
@@ -29,45 +39,54 @@ namespace KorpArhivs.Pages
         public string EventDescription { get; set; }
 
         [BindProperty]
-        public List<string> UploadedFiles { get; set; }
+        public int FileCount { get; set; }
 
-        public async Task OnGet(string category, string id)
+        public async Task OnGet()
         {
-            Category = category;
-            Id = id;
+            var category = (string)RouteData.Values["category"];
+            var id = (string)RouteData.Values["id"];
 
-            var tableName = _configuration["StorageTables:Events"];
-            var containerName = _configuration["StorageBlobs:Events"];
-            var connectionString = _configuration.GetConnectionString("TableStorage");
-
-            var tableClient = new TableClient(connectionString, tableName);
-
-            var deleteEventConfirmationModel = tableClient.GetEntity<TableEntity>(Category, Id);
+            var deleteEventConfirmationModel = _tableClient.GetEntity<TableEntity>(category, id);
 
             EventName = deleteEventConfirmationModel.Value.GetString("Name");
             EventDescription = deleteEventConfirmationModel.Value.GetString("Description");
 
-            // Create a BlobServiceClient object which will be used to create a container client
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            FileCount = (await GetAllBlobs(category, id)).Count;
+        }
 
-            // Create the container and return a container client object
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        public async Task<IActionResult> OnPost()
+        {
+            var category = (string)RouteData.Values["category"];
+            var id = (string)RouteData.Values["id"];
 
+            await _tableClient.DeleteEntityAsync(category, id);
+
+            var uploadedFiles = await GetAllBlobs(category, id);
+
+            foreach (var file in uploadedFiles)
+            {
+                BlobClient blobClient = _blobContainerClient.GetBlobClient(file);
+                await blobClient.DeleteAsync();
+            }
+
+            return RedirectToPage("/Index");
+        }
+
+        private async Task<List<string>> GetAllBlobs(string category, string id)
+        {
+            var result = new List<string>();
             // Call the listing operation and return pages of the specified size.
-            var resultSegment = containerClient.GetBlobsAsync(prefix: $"{category}/{id}").AsPages();
-
-            UploadedFiles = new List<string>();
+            var resultSegment = _blobContainerClient.GetBlobsAsync(prefix: $"{category}/{id}").AsPages();
 
             // Enumerate the blobs returned for each page.
             await foreach (Azure.Page<BlobItem> blobPage in resultSegment)
             {
                 foreach (BlobItem blobItem in blobPage.Values)
                 {
-                    var blobClient = containerClient.GetBlobClient(blobItem.Name);
-                    UploadedFiles.Add(blobClient.Uri.ToString());
+                    result.Add(blobItem.Name);
                 }
-
             }
+            return result;
         }
     }
 }
